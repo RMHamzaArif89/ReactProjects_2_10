c398ef639b47b0c8310fc08e84a6cc3a3b2b8b5c
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-02-24 13:05:32 +0500

    Nav:Created the Components folder and creatd first comoonent of Nav

---
+++ GymSite_Project2/src/Components/nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <div className="img">
+            <img src="/images/logo.jpg" alt="" />
+        </div>
+        <div className="list">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Information</li>
+            </ul>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Nav
