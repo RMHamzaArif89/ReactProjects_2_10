47db699297725916a6819357f33b87fef46e6d5c
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-02-25 18:58:18 +0500

    created the todo component & its css file

---
+++ TodoList_Pro4/src/components/Todo.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import './css/todo.css'
+
+
+function Todo() {
+    const getLocalData=()=>{
+        const lists=localStorage.getItem('todoApp')
+        if(lists){
+          return JSON.parse(lists)
+        }else{
+          []
+        }
+       }
+    let [val,setVal]=useState('')
+    let [item,setItem]=useState(getLocalData)
+    const changeVal=(e)=>{
+        const inpValue=e.target.value
+        setVal(inpValue)
+       
+            
+    }
+    const addValue=()=>{
+        if(!val){
+            alert('enter the value')
+        }else{
+            const newData={
+                id:new Date().getTime().toString(),
+                name:val
+            }
+            setItem([...item,newData])
+            setVal('')
+        }
+       
+    }
+
+    const delFunc=(i)=>{
+        const filterData=item.filter((pre)=>{
+          return (pre.id!==i)
+        })
+        setItem(filterData)
+       
+    }
+    // set localstroage value
+    useEffect(()=>{
+     localStorage.setItem('todoApp',JSON.stringify(item))
+    },[item])
+
+  return (
+    <div className='main'>
+        <div className="con">
+            <img src="/images/todo.jpg" alt="" className="img" />
+            <div className="heading">TodoApp</div>
+           <div className="input">
+           <input type="text" name='' onChange={changeVal}/>
+            <div className="inp-btn" onClick={addValue}>+</div>
+           </div>
+          {item.map((i)=>{
+            return (<li className='li' key={i.id}>{i.name} <i className='bi bi-trash3' onClick={()=>{delFunc(i.id)}}></i></li>)
+
+          })}
+        </div>
+      
+    </div>
+  )
+}
+
+export default Todo
