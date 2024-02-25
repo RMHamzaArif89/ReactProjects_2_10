c554b2ff42e0fc339c3e8ef80bd5cc378f5a8713
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-02-25 08:04:18 +0500

    Created the Second Project of FoodSite using Vite

---
+++ FoodSite_Project2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>okay</h1>
+        
+    </>
+  )
+}
+
+export default App
