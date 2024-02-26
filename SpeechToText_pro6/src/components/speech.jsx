d51bb709ea537f3a39055d25eaa311c96f9ea554
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-02-26 19:30:28 +0500

    using copy copytoclipboard module copy transcript. text of the speech

--- SpeechToText_pro6/src/components/speech.jsx
+++ SpeechToText_pro6/src/components/speech.jsx
@@ -1,15 +1,17 @@
 import 'regenerator-runtime/runtime'
-import React from 'react';
+import React, { useState } from 'react';
+import useClipboard from "react-use-clipboard";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './css/speech.css'
-
+import './css/copy.css'
 
 const Dictaphone = () => {
+    const[text,setText]=useState('')
+    const [isCopied, setCopied] = useClipboard(text);
+
     const StartListening=()=>SpeechRecognition.startListening({ continuous: true ,language:'en'})
     const StopListening=()=>SpeechRecognition.stopListening()
-    const start=()=>{
-    console.log('start')
-    }
+   
   const {
     transcript,
     listening,
@@ -21,7 +23,20 @@ const Dictaphone = () => {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+
+  //copy text functions
+ const copyFunc=()=>{
+    setText(transcript)
+    setCopied(text)
+ }
+
   return (
+    <>
+      <button className='copy' onClick={copyFunc}>
+        Copy To ClipBoard
+     {isCopied ? "Yes! 👍" : "Nope! 👎"}
+    </button>
+
     <div className='con'>
       <div className='microphone'>Microphone: {listening ? 'on' : 'off'}</div>
       <div className='transcript'>{transcript}</div>
@@ -32,6 +47,7 @@ const Dictaphone = () => {
       <button className='reset' onClick={resetTranscript}>Reset</button>
       </div>
     </div>
+    </>
   );
 };
 export default Dictaphone;
