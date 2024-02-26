3c4e04ea2e0ab681b6317ef1ce5bed8cca64dbc1
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-02-26 16:42:18 +0500

    created the sixth project of speechToText using.. Vite

---
+++ SpeechToText_pro6/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Dictaphone from './components/speech'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Dictaphone/>
+    </>
+  )
+}
+
+export default App
