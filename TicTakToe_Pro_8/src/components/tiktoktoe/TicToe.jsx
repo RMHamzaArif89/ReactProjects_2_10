d22a974925b754deb45e264b1dd50435cb8d427b
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-09 18:23:11 +0500

    Created the wining pattern & match the values if.. values are equal then show the winning quote else..other

--- TicTakToe_Pro_8/src/components/tiktoktoe/TicToe.jsx
+++ TicTakToe_Pro_8/src/components/tiktoktoe/TicToe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RxCross1 } from "react-icons/rx";
 import { GiTicTacToe } from "react-icons/gi";
 import { FaRegCircle } from "react-icons/fa";
@@ -6,13 +6,43 @@ import './tictoe.css'
 import { useState } from 'react';
 
 function TicToe() {
-    let[xTurn,setXturn]=useState(false)
+    let[xTurn,setXturn]=useState(true)
     let [value,setValue]=useState(Array(9).fill(''))
+    let [status,setStatus]=useState()
+
+    let winner=(value)=>{
+    let winning= [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+        
+      ];
+      for (let i = 0; i < winning.length; i++) {
+        let[x,y,z]= winning[i]
+
+        if(value[x] && 
+          value[x] === value[y] &&
+          value[x] === value[z]
+          
+          ){return value[x]}
+          
+        
+      }
+      return null
+     
+    }
+
+
     let changState=(e)=>{
 
 
         let takevalues=[...value]
-        if(takevalues[e]) {return}
+        if(winner(takevalues) ||takevalues[e]) {return}
         takevalues[e]=xTurn?'x':'0';
         
 
@@ -26,10 +56,32 @@ function TicToe() {
 
     
     }
+    useEffect(()=>{
+      // winner()
+
+      if(!winner(value) && value.every(item=>item!=='')){
+        setStatus('This is draw! Please try again!')
+        
+      }else if(winner(value)){
+        setStatus(`${winner(value)} is the winner,Congratulations!!!`)
+        
+      }else{
+        setStatus(`next player is ${xTurn?'x':'0'}`)
+      }
+
+    },[xTurn,value])
+
+
+    let refreshFunc=()=>{
+         window.location.reload()
+    }
 
   return (
     <div className='tictoe'>
         <div className="heading">Play the Game <GiTicTacToe/> </div>
+        <div className="heading" style={{color:winner(value)?'green':'blue'}}>{
+          status
+        }</div>
       <div className="con">
 
         <div  className="con-box" onClick={()=>{ changState(0)}}>{value[0]}</div>
@@ -44,7 +96,7 @@ function TicToe() {
    
       </div>
 
-      <div className="refresh-btn">Refresh</div>
+      <div className="refresh-btn" onClick={refreshFunc}>Refresh</div>
     </div>
   )
 }
